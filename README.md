1f99a0a713e4c60b684b3b13499bd11cfd698246
AlexWetzel <wetzela@live.com>
2018-12-12 16:51:38 -0500

    Updated sequelize-cli to latest version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Sequelize-cli-practice
 Just practicing with sequelize cli
+
+https://tech.luc.id/sequelize-it-part-1-4bb752097861
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Sequelize = require('sequelize');
 // const bodyParser = require("body-parser");
 // If this app had routes, they would be imported here
 // const routes = require('./routes/api.js');
@@ -10,9 +11,9 @@ const PORT = process.env.PORT || 3001;
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
 // app.use("", routes);
 
@@ -20,6 +21,30 @@ if (process.env.NODE_ENV === "production") {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // });
 
+// const sequelize = new Sequelize('petsDB', "root", "RootPass", {
+//   host: "127.0.0.1",
+//   dialect: 'mysql',
+//   operatorsAliases: false,
+
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   },
+// });
+
+
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+//   });
+
+
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
-})
+});
